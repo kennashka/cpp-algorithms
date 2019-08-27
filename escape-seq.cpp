a539d8f12dafd9d60f748d5388709eb48cf5868e
Kennashka DeSilva <mail@kennashka.com>
2019-08-27 00:32:13 -0400

    Create escape-seq.cpp

---
+++ escape-seq.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std; 
+ 
+int main() 
+{
+    cout << "The newline escape sequence is \\n" << endl;
+    cout << "The tab character is represented as \'\\t\'" << endl;
+    cout << "The tab character is represented as '\\t'" << endl;
+    cout << "The string \"Sunny\" contains five characters." << endl;
+    
+    return 0;
+}
