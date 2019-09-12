0ab85c8d72cc68f410630fc31dac327d5bd9bf2c
Kennashka DeSilva <mail@kennashka.com>
2019-09-12 11:47:18 -0400

    Create char_data_types.cpp

---
+++ char_data_types.cpp
@@ -0,0 +1,27 @@
+# include <iostream> 
+using namespace std;
+
+
+// logic error gets the one output
+
+int main(){
+  char e, f;
+
+  cout << "Enter single character: ";
+  cin >> e;
+
+cout << "Char character entered: "<< e;
+cout << endl;
+
+ cout << "Enter two character with no spaces: ";
+ cin >> f;
+
+cout << "Char character entered: " << f << endl;
+cout << endl;
+
+
+cout << "Char e and Char f: " << e << " " << f << endl;
+
+return 0; 
+
+} 
