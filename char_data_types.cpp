f24e862ecb96383d97b3053ee788b8b53eba8f49
Kennashka DeSilva <mail@kennashka.com>
2019-09-12 12:21:26 -0400

    Update char_data_types.cpp

--- char_data_types.cpp
+++ char_data_types.cpp
@@ -25,3 +25,157 @@ cout << "Char e and Char f: " << e << " " << f << endl;
 return 0; 
 
 } 
+
+
+
+
+// # include <iostream> 
+// # include <cmath> 
+// using namespace std;
+
+
+// // logic error gets the one output
+
+// int main(){
+//   int num1, num2, sum;
+
+//   cout << "Enter a number with two decimal places followed by a single digit alpha char followed by an int with no space anywhere ";
+//   cin >> num1 >> num2;
+//   sum = pow(num1, num2);
+// cout << num1 << "to the power of" << num2 <<" = " << sum;
+
+
+// return 0; 
+
+// } 
+
+
+
+
+
+// # include <iostream> 
+// # include <iomanip> 
+// using namespace std;
+
+
+// // logic error gets the one output
+
+// int main(){
+//   int a;
+//   float b;
+//   char c;
+
+//   cout << "Enter a number with two decimal places followed by a single digit alpha char followed by an int with no space anywhere ";
+//   cin >> b >> c >> a;
+
+// cout << "values entered: " << fixed << setprecision(2) << b << c << a;
+
+// return 0; 
+
+// } 
+
+
+
+
+
+
+
+
+
+// # include <iostream> 
+// using namespace std;
+
+
+// // logic error gets the one output
+
+// int main(){
+//   float c, d;
+
+//   cout << "Enter a number with two decimal places: ";
+//   cin >> c;
+
+// cout << "Number entered: " << c;
+// cout << endl;
+
+//  cout << "Enter a number with 0 decimals: ";
+//   cin >> d;
+
+// //cout << "float number entered: " << d << endl;
+
+// cout << "float number entered: " << fixed << d<<endl;
+
+// cout << "float number entered: " << setprecision(2) << d <<endl;
+
+// return 0; 
+
+// } 
+
+
+
+
+
+
+
+// # include <iostream> 
+// using namespace std;
+
+
+// // logic error gets the one output
+
+// int main(){
+//   int a, b;
+
+//   cout << "Enter a whole integer number: ";
+//   cin >> a;
+
+// cout << "integer entered: " << a;
+// cout << endl;
+
+//  cout << "Enter a number with two decimals: ";
+//   cin >> b;
+
+
+// cout << "float entered: " << b << endl;
+
+// return 0; 
+
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+// # include <iostream> 
+// using namespace std;
+
+
+// // logic error gets the one output
+
+// int main(){
+//   char e, f;
+
+//   cout << "Enter single character: ";
+//   cin >> e;
+
+// cout << "Char character entered: "<< e;
+// cout << endl;
+
+//  cout << "Enter two character with no spaces: ";
+//  cin >> f;
+
+// cout << "Char character entered: " << f << endl;
+// cout << endl;
+
+
+// cout << "Char e and Char f: " << e << " " << f << endl;
+
+// return 0; 
+
+// } 
