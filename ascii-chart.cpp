7ca43a4a7e69430c08b636e530ac66818d6a9ebb
Kennashka DeSilva <mail@kennashka.com>
2019-10-03 11:05:46 -0400

    Create ascii-chart.cpp

---
+++ ascii-chart.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+        cout << static_cast<char>('A'+ 2) << endl;
+        cout << static_cast<char>(66) << endl;
+ 
+    return 0;
+}
